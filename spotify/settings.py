f814316bc66e976b2ab2c57592e708c640700b58
Ankit Goswami <130369228+askankit07@users.noreply.github.com>
2024-08-25 00:32:40 +0530

    hosts

--- spotify/settings.py
+++ spotify/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ADMINS = [('Ankit goswami', 'askankit07@gmail.com')]
 
 
-ALLOWED_HOSTS = ['https://9hmn06djz0.execute-api.us-west-2.amazonaws.com']
+ALLOWED_HOSTS = ['127.0.0.1','https://9hmn06djz0.execute-api.us-west-2.amazonaws.com']
 
 CSRF_COOKIE_SECURE = True
 
