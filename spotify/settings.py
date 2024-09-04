c8e32c34f8e6dea484a92915df34c044b2cff0ce
Ankit Goswami <130369228+askankit07@users.noreply.github.com>
2024-09-04 12:23:02 +0530

    change db to local

--- spotify/settings.py
+++ spotify/settings.py
@@ -89,29 +89,12 @@ WSGI_APPLICATION = 'spotify.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'root',
-        'PASSWORD': '72zFm6dbvr7Kwxv9',
-        'HOST': 'postgresql-rds-dizzy-cherry-1icn.cz2msosg8tbh.us-west-2.rds.amazonaws.com',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'SpotfiyDatabase',
-#         'USER': 'SuperUser',
-#         'PASSWORD': '#askmahi*07',
-#         'HOST': 'spotifydatabase.czaewuqugvhq.us-east-1.rds.amazonaws.com',
-#         'PORT': 5432
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
